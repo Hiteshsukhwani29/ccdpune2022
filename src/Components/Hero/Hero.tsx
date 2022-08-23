a1c1a8f0162f896cd4153052b9ab74cfdaaba773
Pratik Kale <ppvkale@gmail.com>
2022-08-23 15:31:11 +0530

    Routing for Under Review

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -99,7 +99,8 @@ const Hero = () => {
                       View Tickets
                     </button>
                   ) : (
-                    <button className="transition ease-in-out bg-yellow-500 duration-300 text-white h-fit w-fit text-base py-2 px-4 rounded">
+                    <button className="transition ease-in-out bg-yellow-500 duration-300 text-white h-fit w-fit text-base py-2 px-4 rounded"
+                    onClick={() => navigate('/ccd2022/dashboard')}>
                       Under Review
                     </button>
                   )
