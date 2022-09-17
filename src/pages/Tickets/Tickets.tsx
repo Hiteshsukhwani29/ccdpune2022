05d5dafc34e697b13cd52b5b0973f3228d92b9d4
Pratik Kale <ppvkale@gmail.com>
2022-09-17 19:43:15 +0530

    tickets updated

--- src/pages/Tickets/Tickets.tsx
+++ src/pages/Tickets/Tickets.tsx
@@ -4,12 +4,12 @@ import { db, auth } from '../../services/UserAuth'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { getDoc, doc } from 'firebase/firestore'
 import { getStorage, ref, listAll, getDownloadURL } from 'firebase/storage'
-import {QRCodeSVG} from 'qrcode.react';
-import { QRCode } from 'react-qrcode-logo';
-import Title from '../../Components/Title/Title'
-import ReactDOM from 'react-dom';
-import gdg from '../../Images/qr.png'
-import logo from '../../Images/logo.png'
+// import {QRCodeSVG} from 'qrcode.react';
+// import { QRCode } from 'react-qrcode-logo';
+// import Title from '../../Components/Title/Title'
+// import ReactDOM from 'react-dom';
+// import gdg from '../../Images/qr.png'
+// import logo from '../../Images/logo.png'
 
 const Tickets = () => {
   const [user, loading]: any = useAuthState(auth)
@@ -74,35 +74,48 @@ const Tickets = () => {
         Congratulations on making it through hundreds of applications! <br />
         We look forward to see you at the Cloud Community Days.
       </p>
-      {/* <p className="mb-8 lg:mb-16 font-light text-center text-red-600 sm:text-xl">
-        Workshop passes will start rolling out from 5 August. <br />
-        Check here after 5 August for your Workshop pass.
-      </p> */}
-
-      <p className="text-center">
-                    Share the news with your friends, use hashtags #CCDPune and
-                    #CloudCommunityDays, tag us with @gdgcloudpune (
-                    <a 
-                      target={'_blank'}
-                      rel={'noreferrer'}
-                      href="https://twitter.com/gdgcloudpune">Twitter</a>
-                    ) and stand a chance to win exclusive goodies! 🎉
+      <div className="flex flex-col lg:flex-row my-0 lg:justify-center items-center ">
+      <div className="m-4 flex flex-col items-center bg-gray-100 rounded-lg border shadow-md md:flex-row max-w-sm md:max-w-xl hover:bg-gray-200">
+                <img
+                  className="object-cover w-full max-h-screen border rounded-l-lg md:h-auto md:w-72 md:rounded-none md:rounded-l-lg"
+                  src={PassTicket}
+                  alt="Workshop Pass"
+                />
+                <div className="flex flex-col justify-between p-4 leading-normal">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                    Event Pass
+                  </h5>
+                  <p className="mb-3 font-normal text-gray-700">
+                    &#128197; 24 September, 2022 <br />
+                    &#128205; Conrad, Pune
                   </p>
-
-                  <div className="text-center">
-      <a
+                  <a
                     href={PassTicket}
                     target="_blank"
-                    rel="noopener noreferrer"
-                    className="btn btn-primary me-2 text-center text-white no-underline bg-blue-500 rounded hover:bg-blue-600"
+                    rel="noopener noreferrer" 
+                    download={PassTicket}
+                    className="px-6 py-2 text-center text-white no-underline bg-blue-500 rounded hover:bg-blue-600"
                   >
                     Download
                   </a>
-        <a className="btn btn-primary my-4" href="/ccd2022">
-          Go Home
-        </a>
-      </div>
+                  <br />
+                  <span>
+                    Share the news with your friends, use hashtags #CCDPune and
+                    #CloudCommunityDays, tag us with @gdgcloudpune (
+                    <a target="_blank"
+                    rel="noopener noreferrer" href="https://twitter.com/gdgcloudpune">Twitter</a>,{' '}
+                    <a target="_blank"
+                    rel="noopener noreferrer" href="https://www.linkedin.com/company/gdg-cloud-pune/">
+                      LinkedIn
+                    </a>
+                    ) and stand a chance to win exclusive goodies! 🎉
+                  </span>
+                </div>
+              </div>
+    
 
+           
+          </div>            
       
       <br />
       <br />
