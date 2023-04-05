acb6ac4e8c4a20e26e5f8d5ec2bc5253009f9da9
Hitesh Sukhwani <hiteshsukhwani29@gmail.com>
2023-04-05 23:45:20 +0530

    Merge branch 'main' of https://github.com/Hiteshsukhwani29/ccdpune2022

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,43 @@ import Footer from './Components/Footer/Footer'
 import Loading from './Components/Loading/Loading'
 import Navbar from './Components/Navbar/Navbar'
 import Navigation from './pages/Navigation'
+import ComingSoon from './Images/cs-f1.svg'
+import ComingSoonMobile from './Images/cs-f1m.svg'
+import LinkedInIcon from './Images/linkedin-icon.svg'
+import TwitterIcon from './Images/twitter-icon.svg'
+import YoutubeIcon from './Images/youtube-icon.svg'
+import InstagramIcon from './Images/insta-icon.svg'
+import useWindowSize from './Hooks/useWindowSize'
 
 const Page = () => {
+  const size = useWindowSize();
+  console.log(size);
   return (
     <>
-      <BrowserRouter>
+      <img src={ComingSoon} className=" m-auto h-screen" />
+      <img src={ComingSoonMobile} className=' m-auto h-screen'/>
+      <div className=" absolute bottom-10 left-0 right-0">
+        <div className="font-sans font-semibold mb-5 text-center">
+          Follow our socials to stay updated
+        </div>
+        <div className="flex justify-center">
+          <a href="https://www.linkedin.com/company/gdg-cloud-pune/">
+            <img src={LinkedInIcon} className=" h-10 ml-4 mr-2" />
+          </a>
+          <a href="https://twitter.com/gdgcloudpune">
+            <img src={TwitterIcon} className=" h-10 mx-2" />
+          </a>
+          <a href="https://www.youtube.com/@gdgcloudpune9722">
+            <img src={YoutubeIcon} className=" h-10 mx-2" />
+          </a>
+          <a href="https://www.instagram.com/gdgcloudpune/">
+            <img src={InstagramIcon} className=" h-10 mr-4 ml-2" />
+          </a>
+        </div>
+      </div>
+      {/* <BrowserRouter>
         <Navigation />
-      </BrowserRouter>
+      </BrowserRouter> */}
     </>
   )
 }
